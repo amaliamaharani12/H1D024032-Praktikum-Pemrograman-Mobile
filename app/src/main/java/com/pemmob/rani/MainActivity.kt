3b66014bd21e7596f4d6ff60c086e7d502c441e2
amaliamaharani12 <amlmhrnndssy17@gmail.com>
2026-09-15 21:21:41 +0700

    Pertemuan 2

---
+++ app/src/main/java/com/pemmob/rani/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.pemmob.rani
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.pemmob.rani.ui.screen.BasicInfoScreen
+import com.pemmob.rani.ui.screen.HubungiKamiScreen
+import com.pemmob.rani.ui.theme.PraktikumMobileJualanTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PraktikumMobileJualanTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "basic_info") {
+                        composable(route = "basic_info") {
+                            BasicInfoScreen(
+                                onNavigateToContact = { navController.navigate(route = "form_screen") }
+                            )
+                        }
+                        composable(route = "form_screen") {
+                            HubungiKamiScreen(navController = navController)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
